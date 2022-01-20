799a10835247d36e331e0699144a6ae8e10fa94b
djdenielb <luisdmateos@outlook.com>
2022-01-20 12:00:41 -0600

    Activado el binding en el MainActivity.java

--- app/src/main/java/com/mateoscorp/super_heroes_java/MainActivity.java
+++ app/src/main/java/com/mateoscorp/super_heroes_java/MainActivity.java
@@ -3,12 +3,19 @@ package com.mateoscorp.super_heroes_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import com.mateoscorp.super_heroes_java.databinding.ActivityMainBinding;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
     }
 }
